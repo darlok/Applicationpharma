7b141459210c62e540f8d14ee1b423c69f3f18d8
admin <1132@outlook.fr>
2020-11-12 09:25:25 +0100

    DAO presque fonctionnel

--- app/src/main/java/Modele/Pharmacie.java
+++ app/src/main/java/Modele/Pharmacie.java
@@ -17,46 +17,85 @@ public class Pharmacie {
         this.typeVoie=typeVoie;
         this.Ville=Ville;
         this.voie=voie;
-
     }
-    public String getNoFiness(){return noFiness;}
-    public void setNoFiness(){this.noFiness= noFiness;}
 
-    public String getRaisonSociale(){return raisonSociale;}
-    public void setRaisonSociale(){this.raisonSociale= raisonSociale;}
+    public String getNoFiness()
+    { return noFiness; }
 
-    public String getNumVoie(){return numVoie;}
-    public void setNumVoie(){this.numVoie= numVoie;}
+    public void setNoFiness()
+    { this.noFiness= noFiness; }
 
-    public String getTypeVoie(){return typeVoie;}
-    public void setTypeVoie(){this.typeVoie= typeVoie;}
+    public String getRaisonSociale()
+    { return raisonSociale; }
 
-    public String getVoie(){return voie;}
-    public void setVoie(){this.voie= voie;}
+    public void setRaisonSociale()
+    { this.raisonSociale= raisonSociale; }
 
-    public String getLieuDitBp(){return lieuDitBp;}
-    public void setLieuDitBp(){this.lieuDitBp= lieuDitBp;}
+    public String getNumVoie()
+    { return numVoie; }
 
-    public String getCodeDepartement(){return codeDepartement;}
-    public void setCodeDepartement(){this.codeDepartement= codeDepartement;}
+    public void setNumVoie()
+    { this.numVoie= numVoie; }
 
-    public String getCodePostal(){return codePostal;}
-    public void setCodePostal(){this.codePostal= codePostal;}
+    public String getTypeVoie()
+    { return typeVoie; }
 
-    public String getVille(){return Ville;}
-    public void setVille(){this.Ville= Ville;}
+    public void setTypeVoie()
+    { this.typeVoie= typeVoie; }
 
-    public String getTelephone(){return telephone;}
-    public void setTelephone(){this.telephone= telephone;}
+    public String getVoie()
+    { return voie; }
 
-    public String getTelecopie(){return telecopie;}
-    public void setTelecopie(){this.telecopie= telecopie;}
+    public void setVoie()
+    { this.voie= voie; }
 
-    public String getSiret(){return siret;}
-    public void setSiret(){this.siret= siret;}
+    public String getLieuDitBp()
+    { return lieuDitBp; }
 
-    public Integer getNumCategorie(){return numCategorie;}
-    public void setNumCategorie(){this.numCategorie= numCategorie;}
+    public void setLieuDitBp()
+    { this.lieuDitBp= lieuDitBp; }
+
+    public String getCodeDepartement()
+    { return codeDepartement; }
+
+    public void setCodeDepartement()
+    { this.codeDepartement= codeDepartement; }
+
+    public String getCodePostal()
+    {return codePostal;}
+
+    public void setCodePostal()
+    {this.codePostal= codePostal;}
+
+    public String getVille()
+    {return Ville;}
+
+    public void setVille()
+    {this.Ville= Ville;}
+
+    public String getTelephone()
+    {return telephone;}
+
+    public void setTelephone()
+    {this.telephone= telephone;}
+
+    public String getTelecopie()
+    {return telecopie;}
+
+    public void setTelecopie()
+    {this.telecopie= telecopie;}
+
+    public String getSiret()
+    {return siret;}
+
+    public void setSiret()
+    {this.siret= siret;}
+
+    public Integer getNumCategorie()
+    {return numCategorie;}
+
+    public void setNumCategorie()
+    {this.numCategorie= numCategorie;}
 
 
 }
